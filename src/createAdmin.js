aeba7d3b075ad36d78ea4adba0d2c46879f64b66
AleksMarkov <137721438+AleksMarkov@users.noreply.github.com>
2024-08-20 09:53:43 -0400

    updated userRoutes for login and createAdmin

---
+++ src/createAdmin.js
@@ -0,0 +1,31 @@
+//createAdmin.js
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const User = require("./models/User"); // Проверь путь до модели пользователя
+require("dotenv").config();
+
+const createUser = async () => {
+  await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log("mongoose.connect");
+  const hashedPassword = await bcrypt.hash("300041", 10);
+  console.log("hashedPassword");
+  const user = new User({
+    name: "Admin",
+    email: "soncha@ukr.net",
+    password: hashedPassword,
+  });
+  console.log("user", User);
+  try {
+    await user.save();
+    console.log("Admin user created successfully");
+  } catch (error) {
+    console.error("Error creating admin user:", error);
+  } finally {
+    mongoose.connection.close();
+  }
+};
+
+createUser();
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,18 +2,41 @@
 const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const authMiddleware = require("../middleware/authMiddleware");
 
-// Пример маршрута для создания пользователя
-router.post("/register", async (req, res) => {
-  const { name, email, password } = req.body;
+// Роут для логина
+router.post("/api/user/login", async (req, res) => {
+  const { email, password } = req.body;
 
   try {
-    const newUser = new User({ name, email, password });
-    await newUser.save();
-    res.status(201).json({ message: "User created successfully" });
+    // Поиск пользователя по email
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Неправильный email или пароль" });
+    }
+
+    // Проверка пароля
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Неправильный email или пароль" });
+    }
+
+    // Генерация JWT токена
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({ token });
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    res.status(500).json({ message: error.message });
   }
+});
+
+// Пример защищенного маршрута
+router.get("/api/user/protected", authMiddleware, (req, res) => {
+  res.json({ message: "Доступ разрешен", user: req.user });
 });
 
 module.exports = router;
